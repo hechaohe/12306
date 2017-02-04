25e4933f082c12a2fef6a391aa3146460afa6bda
hechaohe <550121308@qq.com>
2017-02-04 10:06:22 +0800

    Initial Commit

---
+++ test_12306Btn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test_12306Btn
+//
+//  Created by hc on 2017/2/4.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test_12306Btn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test_12306Btn
+//
+//  Created by hc on 2017/2/4.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
